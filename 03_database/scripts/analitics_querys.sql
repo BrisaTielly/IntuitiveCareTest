5389cab3295d4777fefc0b328423318059f0652b
Brisa Silva <brisatielly12@gmail.com>
2025-03-26 23:16:06 -0300

    feat: adiciona script modelo para realizar as querys analíticas requisitadas

---
+++ 03_database/scripts/analitics_querys.sql
@@ -0,0 +1,36 @@
+--ULTIMO TRIMESTRE-----------------------
+select
+	rc.registro_ans,
+    rc.razao_social,
+    descricao,
+    SUM(
+        CAST(REPLACE(REPLACE(dc.vl_saldo_inicial, '.', ''), ',', '.') AS NUMERIC) -
+        CAST(REPLACE(REPLACE(dc.vl_saldo_final, '.', ''), ',', '.') AS NUMERIC)
+    ) AS despesas
+FROM demonstracoes_contabeis dc
+INNER JOIN relatorio_cadop rc ON dc.reg_ans = rc.registro_ans
+WHERE
+    EXTRACT(YEAR FROM dc.data) = 2024
+    AND EXTRACT(QUARTER FROM dc.data) = (SELECT MAX(EXTRACT(QUARTER FROM data)) FROM demonstracoes_contabeis WHERE EXTRACT(YEAR FROM data) = 2024)
+    AND dc.descricao ILIKE '%EVENTOS%SINISTROS%CONHECIDOS%OU%AVISADOS%'
+GROUP BY registro_ans, rc.razao_social, descricao
+ORDER BY despesas DESC
+LIMIT 10;
+
+
+--ULTIMO ANO----------------------
+SELECT
+    rc.registro_ans,
+    rc.razao_social,
+    descricao,
+     SUM(
+         CAST(REPLACE(REPLACE(vl_saldo_inicial, '.', ''), ',', '.') AS NUMERIC) -
+         CAST(REPLACE(REPLACE(vl_saldo_final, '.', ''), ',', '.') AS NUMERIC)
+     ) AS despesas
+ FROM demonstracoes_contabeis dc
+ INNER JOIN relatorio_cadop rc ON dc.reg_ans = rc.registro_ans
+ WHERE EXTRACT(YEAR FROM data) = 2024
+ AND descricao ILIKE '%EVENTOS%SINISTROS%CONHECIDOS%OU%AVISADOS%'
+ GROUP BY registro_ans, rc.razao_social, descricao
+ ORDER BY despesas DESC
+ LIMIT 10;
