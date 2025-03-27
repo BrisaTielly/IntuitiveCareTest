4503806e7d7421232b963f7f4dba07dc85f5ed80
Brisa Silva <brisatielly12@gmail.com>
2025-03-26 23:14:52 -0300

    feat: adiciona script modelo para criação das tabelas requisitadas

---
+++ 03_database/scripts/create_tables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE demonstracoes_contabeis (
+    data DATE,
+    reg_ans TEXT,
+    cd_conta_contabil TEXT,
+    descricao TEXT,
+    vl_saldo_inicial TEXT,
+    vl_saldo_final TEXT
+);
+
+CREATE TABLE relatorio_cadop (
+	registro_ans TEXT,
+	cnpj TEXT,
+	razao_social TEXT,
+	nome_fantasia TEXT,
+	modalidade TEXT,
+	logradouro TEXT,
+	numero TEXT,
+	complemento TEXT,
+	bairro TEXT,
+	cidade TEXT,
+	uf TEXT,
+	cep TEXT,
+	ddd TEXT,
+	telefone TEXT,
+	fax TEXT,
+	endereco_eletronico TEXT,
+	representante TEXT,
+	cargo_representante TEXT,
+	regiao_de_comercializacao TEXT,
+	data_registro_ans TEXT
+);
