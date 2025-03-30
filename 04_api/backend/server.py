6011c67a670a278671f2712595c92c2caa6888c2
Brisa Silva <brisatielly12@gmail.com>
2025-03-29 21:32:42 -0300

    refact(backend): modifica o código para rodar na aws

--- 04_api/backend/server.py
+++ 04_api/backend/server.py
@@ -3,21 +3,30 @@ from fastapi.responses import JSONResponse
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, Session
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI(title="Busca de Operadoras")
 
 origins = [
-    "http://localhost:8080",  # Endereço do frontend
+    "http://localhost:8000",  # URL do frontend local
+    "http://intuitivecarefront.s3-website.us-east-2.amazonaws.com/",  # Endereço do frontend
 ]
+
+#Configurações abertas para fins de facilitação de testes
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,  
     allow_credentials=True,
-    allow_methods=[""],
-    allow_headers=[""],
+    allow_methods=["GET", "POST", "OPTIONS"],  
+    allow_headers=["*"],  
 )
-
+ 
+#Caso queira rodar no seu servidor da aws, descomente a linha abaixo e comente a linha acima e configure a url no .env 
 DATABASE_URL = "postgresql://admin:admin@localhost/intuitive_care"
+# DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
@@ -35,7 +44,6 @@ def search_operadoras(
     maxResults: int = Query(10, le=50, ge=1, description="Número máximo de resultados"),
     db: Session = Depends(get_db)
 ):
-    # Consulta usando full-text search para obter o ranking dos resultados, considerando razao_social e nome fantasia
     sql = text("""
         SELECT *, ts_rank(
             to_tsvector('portuguese', razao_social || ' ' || nome_fantasia),
@@ -48,4 +56,13 @@ def search_operadoras(
     """)
     result = db.execute(sql, {"q": query, "maxResults": maxResults})
     registros = [dict(row._mapping) for row in result]
-    return JSONResponse(content={"results": registros})
+    
+    # Retornar resposta com cabeçalhos CORS
+    return JSONResponse(
+        content={"results": registros},
+        headers={
+            "Access-Control-Allow-Origin": "*",  # Permite qualquer origem
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",  # Métodos permitidos
+            "Access-Control-Allow-Headers": "*",  # Permite todos os cabeçalhos
+        }
+    )
